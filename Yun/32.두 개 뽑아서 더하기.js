6cac410e0f4bc458e1b81aabc8c2098635c11ea0
Yunjae <nuguri206@gmail.com>
2022-09-26 21:56:18 +0900

    no message

---
+++ Yun/32.두 개 뽑아서 더하기.js
@@ -0,0 +1,16 @@
+function solution(numbers) {
+        var answer = [];
+     
+        for (let i = 0; i < numbers.length - 1; i++) {
+            for (let j = i + 1; j < numbers.length; j++) {
+     
+                if ( !answer.includes(numbers[i] + numbers[j]) ) {
+                    answer.push(numbers[i] + numbers[j]);
+                }    
+            } 
+        }
+     
+        answer.sort((a, b) => a - b);
+     
+        return answer;
+    }
